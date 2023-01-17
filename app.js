e67688911157b92e30af5d892a37d70b5905bf52
igeorge <george.haddad77@yahoo.com>
2023-01-17 14:59:04 -0500

    ENV files added.
    - Added dotenv files

--- app.js
+++ app.js
@@ -1,6 +1,24 @@
 const express = require('express');
 const app = express();
 
+// Use dotenv to run local instances with env variables
+require('dotenv').config({path: './.env'})
+
+// Calling ENV variables
+const
+    APP_ID = process.env.APP_ID,
+    APP_SECRET = process.env.APP_SECRET,
+    VERIFY_TOKEN = process.env.VERIFY_TOKEN,
+    NGROK_TOKEN = process.env.NGROK_TOKEN,
+    PORT = process.env.PORT,
+    SERVER_URL = process.env.SERVER_URL // Will change on new instance of ngrok -- Look for alternative that has permanent URLs 
+
+// Error handling for dotenv
+if (!(APP_ID && APP_SECRET && VERIFY_TOKEN && NGROK_TOKEN)) {
+    console.error('Missing environment values.');
+    process.exit(1);
+}
+
 app.get('/getData', function(req, res){
     res.status(200).send({
         success:"true",
@@ -9,7 +27,7 @@ app.get('/getData', function(req, res){
     })
 })
 
-const PORT = 5555
 app.listen(PORT, ()=>{
     console.log(`server running on port no ${PORT}`)
 })
+
